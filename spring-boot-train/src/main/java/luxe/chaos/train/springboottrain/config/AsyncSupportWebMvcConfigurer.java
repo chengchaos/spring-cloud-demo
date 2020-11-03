e6ab0dea90940435928f61bdcbcbf0bb79fe81f1
chengchaos <chengchaos@outlook.com>
2020-11-04 00:01:14 +0800

    使用日志输出信息。

--- spring-boot-train/src/main/java/luxe/chaos/train/springboottrain/config/AsyncSupportWebMvcConfigurer.java
+++ spring-boot-train/src/main/java/luxe/chaos/train/springboottrain/config/AsyncSupportWebMvcConfigurer.java
@@ -1,5 +1,7 @@
 package luxe.chaos.train.springboottrain.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -19,6 +21,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class AsyncSupportWebMvcConfigurer implements WebMvcConfigurer {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AsyncSupportWebMvcConfigurer.class);
 
     @Bean
     public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
@@ -30,15 +33,15 @@ public class AsyncSupportWebMvcConfigurer implements WebMvcConfigurer {
         return executor;
     }
 
-    public TimeoutCallableProcessingInterceptor timeoutCallableProcessingInterceptor() {
+    private TimeoutCallableProcessingInterceptor timeoutCallableProcessingInterceptor() {
         return new TimeoutCallableProcessingInterceptor();
     }
 
     @Override
     public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
-        System.out.println("configureAsyncSupport ... ");
         configurer.setTaskExecutor(threadPoolTaskExecutor());
-        configurer.setDefaultTimeout(60 * 1000L);
+        LOGGER.info("Configure async support ... set default timeout 30 Seconds.");
+        configurer.setDefaultTimeout(30 * 1000L);
         configurer.registerCallableInterceptors(timeoutCallableProcessingInterceptor());
 
     }
